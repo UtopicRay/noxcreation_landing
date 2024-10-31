bd782a991e7b373f40fb8c93c59d21e397f98bc2
UtopicRay <rayrm@gmail.com>
2024-10-31 16:22:27 -0400

    'Add TablerIcon dependency'

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,6 @@
 import { NavigationLink } from "@/types";
 
-
-export const NavigationLinks:NavigationLink[] = [
+export const NavigationLinks: NavigationLink[] = [
   {
     link: "/",
     key: "explore",
@@ -30,3 +29,207 @@ export const NavigationLinks:NavigationLink[] = [
     label: "Log In",
   },
 ];
+
+export const Categories: NavigationLink[] = [
+  {
+    link: "/",
+    key: "user_interface",
+    label: "User Interface",
+  },
+  {
+    link: "/",
+    key: "user_experience",
+    label: "User Experience",
+  },
+  {
+    link: "/",
+    key: "digital_media",
+    label: "Digital Media",
+  },
+  {
+    link: "/",
+    key: "lifestyle",
+    label: "Lifestyle",
+  },
+  {
+    link: "/",
+    key: "programming",
+    label: "Programming",
+  },
+  {
+    link: "/",
+    key: "animation",
+    label: "Animation",
+  },
+];
+
+export const Products: NavigationLink[] = [
+  {
+    link: "/",
+    key: "pricing",
+    label: "Pricing",
+  },
+  {
+    link: "/",
+    key: "overview",
+    label: "Overview",
+  },
+  {
+    link: "/",
+    key: "browse",
+    label: "Browse",
+  },
+  {
+    link: "/",
+    key: "accessibility",
+    label: "Accessibility",
+  },
+  {
+    link: "/",
+    key: "five",
+    label: "Five",
+  },
+  {
+    link: "/",
+    key: "changelog",
+    label: "Changelog",
+  },
+];
+
+export const Solutions: NavigationLink[] = [
+  {
+    link: "/",
+    key: "brainstorming",
+    label: "Brainstorming",
+  },
+  {
+    link: "/",
+    key: "ideation",
+    label: "Ideation",
+  },
+  {
+    link: "/",
+    key: "wireframing",
+    label: "Wireframing",
+  },
+  {
+    link: "/",
+    key: "research",
+    label: "Research",
+  },
+  {
+    link: "/",
+    key: "design",
+    label: "Design",
+  },
+  {
+    link: "/",
+    key: "concept",
+    label: "Concept",
+  },
+];
+export const Resources = [
+  {
+    link: "/",
+    key: "help",
+    label: "Help Center",
+  },
+  {
+    link: "/",
+    key: "blog",
+    label: "Blog",
+  },
+  {
+    link: "/",
+    key: "tutorials",
+    label: "Tutorials",
+  },
+  {
+    link: "/",
+    key: "FAQs",
+    label: "FAQs",
+  },
+  {
+    link: "/",
+    key: "community",
+    label: "Community",
+  },
+  {
+    link: "/",
+    key: "events",
+    label: "Events",
+  },
+];
+export const Support=[
+  {
+    link: "/",
+    key: "contact",
+    label: "Contact Us",
+  },
+  {
+    link: "/",
+    key: "developers",
+    label: "Developers",
+  },
+  {
+    link: "/",
+    key: "documentation",
+    label: "Documentation",
+  },
+  {
+    link: "/",
+    key: "integrations",
+    label: "Integrations",
+  },
+  {
+    link: "/",
+    key: "reports",
+    label: "Reports",
+  },
+  {
+    link: "/",
+    key: "webinar",
+    label: "Webinar",
+  },
+]
+
+export const Company=[
+  {
+    link: "/",
+    key: "about",
+    label: "About",
+  },
+  {
+    link: "/",
+    key: "press",
+    label: "Press",
+  },
+  {
+    link: "/",
+    key: "events",
+    label: "Events",
+  },
+  {
+    link: "/",
+    key: "careers",
+    label: "Careers",
+  },
+  {
+    link: "/",
+    key: "customers",
+    label: "Customers",
+  },
+  {
+    link: "/",
+    key: "partners",
+    label: "Partners",
+  },
+]
+
+
+
+
+
+
+
+
