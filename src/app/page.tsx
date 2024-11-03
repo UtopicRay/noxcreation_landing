2764e8b29973ea7ecfd68b3a3f05540332dcc634
UtopicRay <rayrm@gmail.com>
2024-11-03 00:38:38 -0400

    'Add all sections'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import styles from "./page.module.css";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import QuestionSection from "@/components/QuestionSection/QuestionSection";
 import FavoriteSection from "@/components/FavoriteSection/FavoriteSection";
+import UserSection from "@/components/UserSection/UserSection";
+import StorySection from "@/components/StorySection/StorySection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <HeroSection></HeroSection>
+      <HeroSection/>
       <SecondSection/>
+      <StorySection/>
+      <UserSection/>
       <FavoriteSection/>
       <QuestionSection/>
     </div>
