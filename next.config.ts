305529e2ee6150699260e818fd4910ed559ece01
UtopicRay <rayrm@gmail.com>
2024-11-03 09:50:46 -0500

    'Disable EsLint during build'

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
 };
 
 export default nextConfig;
